df2370361ff233af8409292118a551a828bbe027
Nipun Garg <deathwasp271@gmail.com>
2017-08-02 18:41:52 +0530

    V 1.0

--- ESMovingAverage.py
+++ ESMovingAverage.py
@@ -1,9 +1,10 @@
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = [99.1, 99.2, 99.6, 98.1]
+data = [99.1, 99.2, 99.6, 98.1, 99.7, 98.5, 97.6, 97.9, 98.1, 98.0, 98.7, 98.2, 97.7, 98.4, 97.9, 98.1, 98.3, 99.2, 99.1, 97.4, 97.7, 97.9, 97.6, 97.5, 97.5, 97.7, 97.4, 97.1]
 
-eSC = 2
+eSC = 1.111
 
 def weightedAvg(randomV, weights): # len(randomV) = len(weights)
     tupleArray = [(randomV[i], weights[i]) for i in range(len(randomV))]
@@ -21,4 +22,4 @@ plt.plot(days, data, 'b', label="Actual Weight")
 plt.plot(days, result, 'r', label="ESMAed Weight")
 plt.legend(loc="upper left")
 plt.show()
-plt.savefig("C:\Users\Nipun\Desktop\WeightTimeSeries.png")
+plt.savefig('../WeightTimeSeries.png')
